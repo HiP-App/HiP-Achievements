b3029d69d29b9bfb2e26f632d6c92161cd7ee8aa
Timur <musaevt@yahoo.com>
2017-10-16 11:15:04 +0200

    change place of dummy data

--- HiP-Achievements/Controllers/AchievementsController.cs
+++ HiP-Achievements/Controllers/AchievementsController.cs
@@ -11,6 +11,43 @@ namespace PaderbornUniversity.SILab.Hip.Achievements.Controllers
     [Route("api/[controller]")]
     public class AchievementsController : Controller
     {
+        AchievementResult achievement1 = new AchievementResult
+        {
+            Id = 1,
+            Description = "Visit 10 exhibits to get this achievement",
+            Title = "Visit 10 exhibits",
+            NextId = 2,
+            Status = AchievementStatus.Published,
+            Type = AchievementType.ExhibitVisited
+        };
+        AchievementResult achievement2 = new AchievementResult
+        {
+            Id = 2,
+            Description = "Visit 20 exhibits to get this achievement",
+            Title = "Visit 20 exhibits",
+            NextId = 3,
+            Status = AchievementStatus.Published,
+            Type = AchievementType.ExhibitVisited
+        };
+        AchievementResult achievement3 = new AchievementResult
+        {
+            Id = 3,
+            Description = "Visit 30 exhibits to get this achievement",
+            Title = "Visit 40 exhibits",
+            NextId = -1,
+            Status = AchievementStatus.Published,
+            Type = AchievementType.ExhibitVisited
+        };
+        AchievementResult achievement4 = new AchievementResult
+        {
+            Id = 1,
+            Description = "Visit all exhibits on the Karls Route to get this achievement",
+            Title = "Finish Karls Route",
+            NextId = -1,
+            Status = AchievementStatus.Unpublished,
+            Type = AchievementType.RouteFinished
+        };
+
         [HttpGet("ids")]
         [ProducesResponseType(200)]
         public IActionResult GetAllAchievements()
@@ -27,42 +64,7 @@ namespace PaderbornUniversity.SILab.Hip.Achievements.Controllers
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var achievement1 = new AchievementResult
-            {
-                Id = 1,
-                Description = "Visit 10 exhibits to get this achievement",
-                Title = "Visit 10 exhibits",
-                NextId = 2,
-                Status = AchievementStatus.Published,
-                Type = AchievementType.ExhibitVisited
-            };
-            var achievement2 = new AchievementResult
-            {
-                Id = 2,
-                Description = "Visit 20 exhibits to get this achievement",
-                Title = "Visit 20 exhibits",
-                NextId = 3,
-                Status = AchievementStatus.Published,
-                Type = AchievementType.ExhibitVisited
-            };
-            var achievement3 = new AchievementResult
-            {
-                Id = 3,
-                Description = "Visit 30 exhibits to get this achievement",
-                Title = "Visit 40 exhibits",
-                NextId = -1,
-                Status = AchievementStatus.Published,
-                Type = AchievementType.ExhibitVisited
-            };
-            var achievement4 = new AchievementResult
-            {
-                Id = 1,
-                Description = "Visit all exhibits on the Karls Route to get this achievement",
-                Title = "Finish Karls Route",
-                NextId = -1,
-                Status = AchievementStatus.Unpublished,
-                Type = AchievementType.RouteFinished
-            };
+            //TODO here filtering
 
             var achievements = new[] { achievement1, achievement2, achievement3, achievement4 };
 
