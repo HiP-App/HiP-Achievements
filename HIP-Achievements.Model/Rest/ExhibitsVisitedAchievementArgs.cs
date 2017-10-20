b7616ed680c4ea02e66655cc3c015afb600bdbd5
Michél Burkhardt <mburkhardt@live.de>
2017-10-20 17:33:58 +0200

    Added Required Attribute

--- HIP-Achievements.Model/Rest/ExhibitsVisitedAchievementArgs.cs
+++ HIP-Achievements.Model/Rest/ExhibitsVisitedAchievementArgs.cs
@@ -1,9 +1,11 @@
-﻿using PaderbornUniversity.SILab.Hip.Achievements.Model.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using PaderbornUniversity.SILab.Hip.Achievements.Model.Entity;
 
 namespace PaderbornUniversity.SILab.Hip.Achievements.Model.Rest
 {
     public class ExhibitsVisitedAchievementArgs : AchievementArgs
     {
+        [Required]
         public int Count { get; set; }
         public override Achievement CreateAchievement()
         {
