e44f75cb35e888b5589817cb676a26a286bc0cf4
Timur <musaevt@yahoo.com>
2017-10-31 23:17:04 +0100

    no message

--- HiP-Achievements/Controllers/ActionControllers/ExhibitVisitedController.cs
+++ HiP-Achievements/Controllers/ActionControllers/ExhibitVisitedController.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PaderbornUniversity.SILab.Hip.EventSourcing.EventStoreLlp;
 
 
 namespace PaderbornUniversity.SILab.Hip.Achievements.Controllers.ActionControllers
