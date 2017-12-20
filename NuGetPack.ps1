773d772fea82d03969839e1a9db5a35ecaefe80f
Michél Burkhardt <mburkhardt@live.de>
2017-12-20 16:45:32 +0100

    Fixed NuGet script and increased version

--- NuGetPack.ps1
+++ NuGetPack.ps1
@@ -3,7 +3,6 @@ Switch ("$env:Build_SourceBranchName")
 {
     "master" { dotnet pack "$csproj" -o . }
     "develop" { dotnet pack "$csproj" -o . --version-suffix "develop" }
-	"iss-hipcms-876" { dotnet pack "$csproj" -o . --version-suffix "test" }
     default { exit }
 }
 $nupkg = (ls *.Sdk\*.nupkg).FullName
